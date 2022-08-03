a4e71f6d98cd817fb3d747e1783948aa5d35f22c
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-08-04 01:20:18 +0530

    feat : category filter

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,11 +1,38 @@
+export const Categories = ({ filter, setFilter }) => {
+	const categoryClickHandler = (event) => {
+		setFilter(event.target.innerText);
+	};
 
-export const Categories = () => {
 	return (
 		<div className="video__cat">
-			<button className="btns video__cat-btn margin-8px">All</button>
-			<button className="btns video__cat-btn margin-8px">Tutorials</button>
-			<button className="btns video__cat-btn margin-8px">Customization</button>
-			<button className="btns video__cat-btn margin-8px">Distros</button>
+			<button
+				className={`btns video__cat-btn margin-8px ${
+					filter === "All" && "video__cat-active"
+				}`}
+				onClick={categoryClickHandler}>
+				All
+			</button>
+			<button
+				className={`btns video__cat-btn margin-8px ${
+					filter === "Tutorial" && "video__cat-active"
+				}`}
+				onClick={categoryClickHandler}>
+				Tutorial
+			</button>
+			<button
+				className={`btns video__cat-btn margin-8px ${
+					filter === "Customization" && "video__cat-active"
+				}`}
+				onClick={categoryClickHandler}>
+				Customization
+			</button>
+			<button
+				className={`btns video__cat-btn margin-8px ${
+					filter === "Distros" && "video__cat-active"
+				}`}
+				onClick={categoryClickHandler}>
+				Distros
+			</button>
 		</div>
-	)
-}
+	);
+};
--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Categories, PlaylistModal, VideoCard } from "../../components";
 import { usePlaylist, useVideo } from "../../contexts";
 import "./Explore.css";
@@ -6,18 +7,30 @@ export const Explore = () => {
 	const {
 		videoState: { videos },
 	} = useVideo();
+
 	const {
 		playlistState: { modalFlag },
 	} = usePlaylist();
+
+	const [filter, setFilter] = useState("All");
+
+	const filterVideos = (filter, videos) => {
+		if (filter === "All") {
+			return videos;
+		} else {
+			return videos.filter((video) => video.category === filter);
+		}
+	};
+	const finalVideosList = filterVideos(filter, videos);
 
 	return (
 		<div className="container__main container__explore">
 			{modalFlag && <PlaylistModal showPlaylistFlag={true} />}
 			<div className="container__mid-sec ">
-				<Categories />
+				<Categories setFilter={setFilter} filter={filter} />
 				<div className="video__list-sec">
 					{videos &&
-						videos.map((eachVideo) => {
+						finalVideosList?.map((eachVideo) => {
 							return (
 								<VideoCard
 									video={eachVideo}
