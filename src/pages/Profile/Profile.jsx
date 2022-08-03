42302284bd4de5bb29f51daac24cb51da98d3855
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-08-03 13:36:16 +0530

    added profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,37 @@
+import { FaUserCircle } from "react-icons/fa";
+import { useAuth } from "../../contexts";
 
 export const Profile = () => {
-    return (
-        <div>User profile page</div>
-    )
-}
+	const userData = JSON.parse(localStorage.getItem("userData"));
+	const { signOutHandler } = useAuth();
+	return (
+		<div className="container__mid-sec">
+			<div className="video__cat">
+				<p>User Profile </p>
+			</div>
+			<div className="center__flex flex__dir-col text-mid">
+				<div>
+					<FaUserCircle className="h1 user__avatar text-primary" />
+					<p className="margin-4px">
+						Name:
+						<span className="text-small txt-primary margin__lr-4px">
+							{" "}
+							{userData?.userName}
+						</span>
+					</p>
+					<p className="margin-4px">
+						E-mail:{" "}
+						<span className="text-small margin__lr-4px">
+							{userData?.email}
+						</span>
+					</p>
+					<button
+						className="btns btn__secondary margin__tb-8px"
+						onClick={signOutHandler}>
+						Logout
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
